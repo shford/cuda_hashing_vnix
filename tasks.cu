1824fdb4b8496509ac44355edab5e0fcf651937b
Hampton Ford <stevenhford3@gmail.com>
2022-07-15 19:42:41 -0600

    attempt append fix

--- tasks.cu
+++ tasks.cu
@@ -286,159 +286,166 @@ __device__ void Md5Calculate_dev(void  const* Buffer, uint32_t BufferSize, MD5_H
 
 
 // statically initialized global variables
-__device__ uint8_t d_num_collisions_found = 0;              // track number of collisions found by active kernel
-__device__ unsigned long long d_collision_attempts = 0;     // track approximate cumulative number of attempts
-__device__ int d_collision_flag = FALSE;                    // signal host to read
-__device__ int global_mutex = UNLOCKED;                     // signal mutex to other threads (globally)
-__device__ unsigned long long random_seeder = 0;            // increment before each rand call to avoid repeat deterministic "randoms"
+__device__ volatile uint8_t d_num_collisions_found = 0;              // track number of collisions found by active kernel
+__device__ volatile unsigned long long d_collision_attempts = 0;     // track approximate cumulative number of attempts
+__device__ volatile int d_collision_flag = FALSE;                    // signal host to read
+__device__ volatile int global_mutex = UNLOCKED;                     // signal mutex to other threads (globally)
+__device__ volatile unsigned long long global_seeder = 0;            // increment before each rand call to avoid repeat deterministic "randoms"
 
 
 // dynamically initialized in host
-__constant__ __device__ MD5_HASH_dev d_const_md5_digest;    // store digest on L2 or L1 cache (on v8.6)
-__device__ unsigned long long d_collision_size;             // track # of characters in collision
+__constant__ __device__ volatile MD5_HASH_dev d_const_md5_digest;    // store digest on L2 or L1 cache (on v8.6)
+__device__ volatile unsigned long long d_collision_size;             // track # of characters in collision
 
 __global__ void find_collisions(volatile char* collision) {
     //===========================================================================================================
     // DECLARATIONS & INITIALIZATION
     //===========================================================================================================
-    printf("Entering thread %d.\n", threadIdx.x);
+    printf("\n...Entering thread %d.\n", threadIdx.x);
 
-    // create warp synchronization semaphore
+    // create warp synchronization variables
     int sync_warp_flag = FALSE;
     int lane_id_of_found_collision = -1;
 
     // allocate local buffer todo make this type a macro
-    char local_collision[INITIAL_COLLISION_BUFF_SIZE];    
+    char local_collision[INITIAL_COLLISION_BUFF_SIZE];
 
     // initialize local buffer
     unsigned long long local_collision_size = d_collision_size;
     for (int byte_index = 0; byte_index <= local_collision_size; ++byte_index) {
         local_collision[byte_index] = collision[byte_index];
     }
+    printf("Initialized local collision storage: %s.\n", local_collision);
+    printf("Local storage currently using %llu characters.\n", local_collision_size);
 
     // allocate room for new digest
     MD5_HASH_dev local_md5_digest;
 
     // allocate storage for random character
-    unsigned long long random_index = NUM_RANDOMS;
     RAND_TYPE randoms[NUM_RANDOMS];
+    unsigned long long random_index = NUM_RANDOMS;
+    printf("Initialized randoms.\n");
 
     //===========================================================================================================
     // COMPUTATIONS - GENERATE RANDS, RESIZE BUFFER, APPEND CHAR, HASH, COMPARE { EXIT }
     //===========================================================================================================
 
-    do
+    while (d_num_collisions_found < TARGET_COLLISIONS)
     {
-        while ( random_index < NUM_RANDOMS)
+        // generate a new batch of random numbers and append a random char to collision
+        if (random_index == NUM_RANDOMS)
         {
-            // generate a new batch of random numbers and append a random char to collision
-            if (random_index == 0) {
-                // generate a new batch of randoms todo: make each random unique to the set
-                random_index = 0;
-                for (int i = 0; i < FUNCTION_CALLS_NEEDED; ++i)
-                {
-                    curandStatePhilox4_32_10_t state;
-                    ++random_seeder;
-                    curand_init(i, random_seeder, 0, &state);
-                    // effectively assigns 4 random uint8_t's per execution
-                    *((uint32_t*)(randoms + i*RAND_BIT_MULTIPLE)) = curand(&state);
-                }
-
-                /*
-                // resize local_collision
-                if (local_collision_size == local_collision_size) {
-                    // retain ptr to old buffer
-                    char* old_buff = local_collision;
-
-                    // reassign local_collision ptr to new buffer
-                    local_buff_size *= 2;
-                    cudaMalloc(&local_collision, local_buff_size);
-
-                    // copy data from old buffer to new buffer
-                    for (int i = 0; i < ARBITRARY_MAX_BUFF_SIZE; ++i) {
-                        local_collision[i] = old_buff[i];
-                    }
-
-                    // free original buffer
-                    //cudaFree(old_buff);
-                }
-                */
-
-                // permanently append a random char from the final position in randoms[]
-                ++local_collision_size;
+            // generate a new batch of randoms todo: make each random unique to the set
+            random_index = 0;
+            for (int i = 0; i < FUNCTION_CALLS_NEEDED; ++i) {
+                curandStatePhilox4_32_10_t state;
+                int tid = threadIdx.x;  // randomize w/in warps (w/in lockstep)
+                ++global_seeder;        // randomize between warps (and blocks)
+                curand_init(tid+global_seeder, i, 0, &state);
+                // effectively assigns 4 random uint8_t's per execution
+                *((uint32_t *) (randoms + i * RAND_BIT_MULTIPLE)) = curand(&state);
             }
 
-            // try (1..2^NUM_ENCODING_BITS-1) for local collision index
-            for (int c = 1; c < CHAR_SET_SIZE; ++c) {
-                // temporary append byte to try
-                local_collision[local_collision_size - 1] = randoms[random_index] ? randoms[random_index] : (uint8_t)1; // no premature null terminators
-                local_collision[local_collision_size] = '\0';
+            /*
+            // resize local_collision
+            if (local_collision_size == local_collision_size) {
+                // retain ptr to old buffer
+                char* old_buff = local_collision;
 
-                // generate new hash
-                Md5Calculate_dev((const void*)local_collision, local_collision_size, &local_md5_digest);
-                ++d_collision_attempts;
+                // reassign local_collision ptr to new buffer
+                local_buff_size *= 2;
+                cudaMalloc(&local_collision, local_buff_size);
 
-                // we found a collision!
-                if ((*((uint32_t*)&d_const_md5_digest) >> 12 == *((uint32_t*)&local_md5_digest) >> 12) || sync_warp_flag == TRUE)
-                {
-                    printf("%d thread id found hash first.\n", threadIdx.x);
-
-                    // synchronize all threads in warp, and get "value" from lane 0
-                    if (lane_id_of_found_collision == -1)
-                    {
-                        lane_id_of_found_collision = threadIdx.x & (WARP_SIZE - 1);
-                        __shfl_sync(0xffffffff, lane_id_of_found_collision, lane_id_of_found_collision);
-                    }
-
-                    // synchronize all threads in warp, and get "value" from lane 0
-                    sync_warp_flag = TRUE;
-                    __shfl_sync(0xffffffff, sync_warp_flag, lane_id_of_found_collision);
-
-                    // threads will converge so that critical thread is not indefinitely suspended
-                    while (sync_warp_flag)
-                    {
-                        // do mutex operations
-                        if ((threadIdx.x & 0x1f) == lane_id_of_found_collision)
-                        {
-                            printf("Thread %d in lane %d entered warp mutex.\n", threadIdx.x, lane_id_of_found_collision);
-                            // set mutex lock
-                            do {} while (atomicCAS(&global_mutex, UNLOCKED, LOCKED));
-
-                            // enter critical section - writing for host polls and signalling when ready
-                            for (int byte_index = 0; byte_index <= local_collision_size; ++byte_index) {
-                                collision[byte_index] = local_collision[byte_index];
-                            }
-                            d_collision_size = local_collision_size;
-
-                            // signal host to read
-                            d_collision_flag = TRUE;
-
-                            // free lock only once host signals finished reading (e.g. d_collision_flag = FALSE)
-                            do {
-                                // thread idles while host updates
-                                // 1) d_num_collisions_found
-                                // 2) d_collision_flag
-                            } while (d_collision_flag);
-
-                            // safely unlock mutex by writing to flag - remember relaxed ordering doesn't matter here
-                            atomicExch(&global_mutex, UNLOCKED);
-
-                            // release non-critical warp threads and reset flag
-                            sync_warp_flag = FALSE;
-                            __shfl_sync(0xffffffff, sync_warp_flag, lane_id_of_found_collision);
-                        }
-                        else
-                        {
-                            // have non-critical warp threads read check for
-                        }
-                        __syncwarp(); // causes the executing thread to wait until all threads specified in mask have executed a __syncwarp()
-                    }
+                // copy data from old buffer to new buffer
+                for (int i = 0; i < ARBITRARY_MAX_BUFF_SIZE; ++i) {
+                    local_collision[i] = old_buff[i];
                 }
+
+                // free original buffer
+                //cudaFree(old_buff);
             }
-            // on next iter: try next random || gen new randoms and append last random to local_collision
-            ++random_index;
+            */
+
+            // permanently append a random char from the final position in randoms[]
+            ++local_collision_size;
         }
-    } while(d_num_collisions_found < TARGET_COLLISIONS);
+
+        // try (1..2^NUM_ENCODING_BITS-1) for local collision index
+
+        for (int c = 1; c < CHAR_SET_SIZE; ++c) {
+            // try charset
+            local_collision[local_collision_size - 1] = c;
+            local_collision[local_collision_size] = '\0';
+
+            // generate new hash
+            Md5Calculate_dev((const void *) local_collision, local_collision_size, &local_md5_digest);
+            for (int i = 0; i < MD5_HASH_SIZE / 2; ++i) {
+                printf("%2.2x", local_md5_digest.bytes[i]);
+            }
+            printf(" | ");
+            for (int i = 0; i < MD5_HASH_SIZE / 2; ++i) {
+                printf("%2.2x", d_const_md5_digest.bytes[i]);
+            }
+            printf(" | Attempt: %llu\n", d_collision_attempts);
+            ++d_collision_attempts;
+
+            // we found a collision!
+            if ((*((uint32_t *) &d_const_md5_digest) >> 12 == *((uint32_t *) &local_md5_digest) >> 12) || sync_warp_flag == TRUE) {
+                printf("%d thread id found hash first.\n", threadIdx.x);
+
+                // synchronize all threads in warp, and get "value" from lane 0
+                if (lane_id_of_found_collision == -1) {
+                    lane_id_of_found_collision = threadIdx.x & (WARP_SIZE - 1);
+                    __shfl_sync(0xffffffff, lane_id_of_found_collision, lane_id_of_found_collision);
+                }
+
+                // synchronize all threads in warp, and get "value" from lane 0
+                sync_warp_flag = TRUE;
+                __shfl_sync(0xffffffff, sync_warp_flag, lane_id_of_found_collision);
+
+                // threads will converge so that critical thread is not indefinitely suspended
+                while (sync_warp_flag) {
+                    // do mutex operations
+                    if ((threadIdx.x & 0x1f) == lane_id_of_found_collision) {
+                        printf("Thread %d in lane %d entered warp mutex.\n", threadIdx.x, lane_id_of_found_collision);
+                        // set mutex lock
+                        do {} while (atomicCAS((int*)&global_mutex, UNLOCKED, LOCKED));
+
+                        // enter critical section - writing for host polls and signalling when ready
+                        for (int byte_index = 0; byte_index <= local_collision_size; ++byte_index) {
+                            collision[byte_index] = local_collision[byte_index];
+                        }
+                        d_collision_size = local_collision_size;
+
+                        // signal host to read
+                        d_collision_flag = TRUE;
+
+                        // free lock only once host signals finished reading (e.g. d_collision_flag = FALSE)
+                        do {
+                            // thread idles while host updates
+                            // 1) d_num_collisions_found
+                            // 2) d_collision_flag
+                        } while (d_collision_flag);
+
+                        // safely unlock mutex by writing to flag - remember relaxed ordering doesn't matter here
+                        atomicExch((int*)&global_mutex, UNLOCKED);
+
+                        // release non-critical warp threads and reset flag
+                        sync_warp_flag = FALSE;
+                        __shfl_sync(0xffffffff, sync_warp_flag, lane_id_of_found_collision);
+                    } else {
+                        // have non-critical warp threads read check for
+                    }
+                    __syncwarp(); // causes the executing thread to wait until all threads specified in mask have executed a __syncwarp()
+                }
+            }
+        }
+
+        // on next iter: try next random || gen new randoms and append last random to local_collision
+        local_collision[local_collision_size - 1] = randoms[random_index];
+        local_collision[local_collision_size] = '\0';
+        ++random_index;
+    }
 }
 
 void task1() {
@@ -476,6 +483,10 @@ void task1() {
     printf("Full MD5 md5_digest is: %s\n", hash);
     printf("TinyHash md5_digest is: %s\n\n", tiny_hash);
 
+    //===========================================================================================================
+    // BEGIN CUDA PARALLELIZATION
+    //===========================================================================================================
+
     // allocate storage for collisions once found
     uint8_t h_collision_index = 0;
     char* h_collisions[TARGET_COLLISIONS];
@@ -486,35 +497,26 @@ void task1() {
         h_collision_sizes[i] = 0;
     }
     int h_collision_flag = FALSE;
-
-    //===========================================================================================================
-    // BEGIN CUDA PARALLELIZATION
-    //===========================================================================================================
-
-    int devices = 0;
-    cudaGetDeviceCount(&devices);
-
-    if (devices < 1) {
-        printf("No CUDA devices found!");
-        exit(0);
-    }
-    // initialize device
-    cudaSetDevice(0);
+    printf("...Allocated host variables.\n");
 
     // allocate global mem for collision - initialized in loop
     volatile char* d_collision;
     gpuErrchk( cudaMalloc((void **)&d_collision, INITIAL_COLLISION_BUFF_SIZE) );
     gpuErrchk( cudaMemset((void*)d_collision, 0x00, INITIAL_COLLISION_BUFF_SIZE));
+    printf("...Allocated device collision.\n");
 
     // parallelization setup - initialize device globals
     gpuErrchk( cudaMemcpyToSymbol(d_const_md5_digest, &md5_digest, sizeof(md5_digest), 0, cudaMemcpyHostToDevice) );
     gpuErrchk( cudaMemcpyToSymbol(d_collision_size, &h_sampleFile_buff_size, sizeof(h_sampleFile_buff_size), 0, cudaMemcpyHostToDevice) );
     gpuErrchk( cudaMemcpy((void*)d_collision, h_sampleFile_buff, h_sampleFile_buff_size, cudaMemcpyHostToDevice) );
+    printf("...Initialized dynamic device variables.\n");
 
     // execution configuration (sync device)
+    printf("...Launching kernel.\n");
     find_collisions<<<BLOCKS_PER_KERNEL, THREADS_PER_BLOCK>>>(d_collision);
+    printf("Done launching kernel.\n");
 
-    // run kernel
+    // poll kernel
     while (h_collision_index < TARGET_COLLISIONS)
     {
         // poll collision flag
@@ -525,16 +527,22 @@ void task1() {
         }
 
         // read updated collision size, collision
+        printf("...Reading collision.\n");
         gpuErrchk( cudaMemcpyFromSymbol(&h_collision_sizes[h_collision_index], d_collision_size, sizeof(h_collision_sizes[h_collision_index]), 0, cudaMemcpyDeviceToHost));
         gpuErrchk( cudaMemcpy(&h_collisions[h_collision_index], (const void*)d_collision, h_collision_sizes[h_collision_index], cudaMemcpyDeviceToHost) );
+        printf("Done.\n\n");
 
         // increment collision index for host and device
+        printf("...Incrementing collision counter.\n");
         ++h_collision_index;
         gpuErrchk( cudaMemcpyToSymbol(d_num_collisions_found, &h_collision_index, sizeof(h_collision_index), 0, cudaMemcpyHostToDevice));
+        printf("Done.\n\n");
 
         // reset flags to release device-wide mutex and reset kernel
+        printf("...Resetting device flag.\n");
         h_collision_flag = FALSE;
         cudaMemcpyToSymbol(d_collision_flag, &h_collision_flag, sizeof(h_collision_flag), 0, cudaMemcpyHostToDevice);
+        printf("Done.\n\n");
     }
     
     // read the final number of cumulative hashing attempts
@@ -544,12 +552,18 @@ void task1() {
             sizeof(h_collision_attempts), 0, cudaMemcpyDeviceToHost));
     }
 
+    printf("...Synchronizing threads.\n");
     gpuErrchk( cudaDeviceSynchronize() );
+    printf("Done.\n\n");
+    printf("...Resetting device.\n");
     gpuErrchk( cudaDeviceReset() );
+    printf("Done.\n\n");
 
     // free collisions
+    printf("...Freeing memory.\n");
     cudaFree((void*)d_collision);
     free(h_sampleFile_buff);
+    printf("Done.\n\n");
 
     printf("\nCalculated %d collisions in %lld attempts... Success!/\n", TARGET_COLLISIONS, h_collision_attempts);
 
